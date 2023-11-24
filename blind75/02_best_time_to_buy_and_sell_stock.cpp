0378d249ae9e6586ecc50e40ac072f1b6e3d2eaa
SanyaNya <sanyanya4ever@gmail.com>
2023-11-24 13:30:25 +0600

    Add blind75 #02 best time to buy and sell stock

---
+++ blind75/02_best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class Solution {
+public:
+  static int maxProfit(const std::vector<int>& prices)
+  {
+    int min = prices.front();
+    int max = prices.front();
+    int max_profit = max - min;
+
+    for(int p : prices)
+    {
+      if(p < min)
+      {
+        //after finding new min reset max
+        min = p;
+        max = p;
+      }
+      else if(p > max)
+      {
+        //check profit after finding new max
+        max = p;
+        const int profit = max - min;
+        if(profit > max_profit)
+            max_profit = profit;
+      }
+    }
+
+    return max_profit;
+  }
+};
