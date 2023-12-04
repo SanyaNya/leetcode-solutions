175920f8c02488e217d403f4fe7347239ed9d29f
SanyaNya <sanyanya4ever@gmail.com>
2023-12-04 12:46:47 +0600

    Add blind75 #06 maximum product subarray

---
+++ blind75/06_maximum_product_subarray.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+//https://leetcode.com/problems/maximum-product-subarray/
+
+//space complexity: O(1) no extra allocations
+
+//time complexity: O(n) iterating once over nums
+
+class Solution {
+public:
+    static int maxProduct(const std::vector<int>& nums) noexcept
+    {
+        int positive_product = 1; //positive product
+        int negative_product = 1; //negative product
+        int max_p = nums.front();
+
+        for(int n : nums)
+        {
+            //if n is negative then positive_product*n -> negative_product and negative_product*n -> positive_product
+            if(n < 0) std::swap(positive_product, negative_product);
+
+            //calculate product for positive and negative subarray
+            //or start new subarray
+            positive_product *= std::max(n, positive_product*n);
+            negative_product *= std::min(n, negative_product*n);
+
+            //check new maximum
+            max_p = std::max(max_p, positive_product);
+        }
+
+        return max_p;
+    }
+};
